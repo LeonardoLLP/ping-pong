43901e52a0cc475e4e135b02f3579806c0bb3643
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-11-30 11:58:23 +0100

    Inicializacion de pygame

--- paso1.py
+++ paso1.py
@@ -7,3 +7,10 @@ VENTANA_HORI = 800  # Ancho de la ventana
 VENTANA_VERT = 600  # Alto de la ventana
 FPS = 60  # Fotogramas por segundo
 BLANCO = (255, 255, 255)  # Color del fondo de la ventana (RGB)
+
+# Inicialización de Pygame
+    pygame.init()
+
+    # Inicialización de la superficie de dibujo (display surface)
+    ventana = pygame.display.set_mode((VENTANA_HORI, VENTANA_VERT))
+    pygame.display.set_caption("Pong 1")
