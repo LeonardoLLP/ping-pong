e9b18aaf271a45a3a8d3b6f550b9a612f549aee1
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-11-30 11:55:59 +0100

    Definicion de constantes

--- paso1.py
+++ paso1.py
@@ -1,3 +1,9 @@
 # Importación de módulos
 import pygame
 from pygame.locals import *
+
+# Definición de constantes
+VENTANA_HORI = 800  # Ancho de la ventana
+VENTANA_VERT = 600  # Alto de la ventana
+FPS = 60  # Fotogramas por segundo
+BLANCO = (255, 255, 255)  # Color del fondo de la ventana (RGB)
