e4b2c18086c807930c5b1089b91debac62cb840f
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-11-30 12:03:20 +0100

    metodo reiniciar

---
+++ paso4.py
@@ -0,0 +1,7 @@
+# Método reiniciar()
+def reiniciar(self):
+        self.x = VENTANA_HORI / 2 - self.ancho / 2
+        self.y = VENTANA_VERT / 2 - self.alto / 2
+        self.dir_x = -self.dir_x
+        self.dir_y = random.choice([-5, 5])
+
