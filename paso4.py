700c3be5b4c734f22ea27e83cff2fc1380847906
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-11-30 12:04:41 +0100

    metodo rebotar

--- paso4.py
+++ paso4.py
@@ -5,3 +5,13 @@ def reiniciar(self):
         self.dir_x = -self.dir_x
         self.dir_y = random.choice([-5, 5])
 
+#Método rebotar()
+def rebotar(self):
+        if self.x <= -self.ancho:
+            self.reiniciar()
+        if self.x >= VENTANA_HORI:
+            self.reiniciar()
+        if self.y <= 0:
+            self.dir_y = -self.dir_y
+        if self.y + self.alto >= VENTANA_VERT:
+            self.dir_y = -self.dir_y
