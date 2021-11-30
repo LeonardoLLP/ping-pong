0ec4bbaf9b62cc0ebe3ab0d9d99145976c7eb04b
paulaanb <paulanb2000@gmail.com>
2021-11-30 11:54:49 +0100

    Definimos las constantes para la inicialización de la superficie de dibujo

--- Paso 2.py
+++ Paso 2.py
@@ -4,3 +4,9 @@
 import random
 import pygame
 from pygame.locals import QUIT
+
+#Definimos las constantes para la inicialización de la superficie de dibujo
+VENTANA_HORI = 800  # Ancho de la ventana
+VENTANA_VERT = 600  # Alto de la ventana
+FPS = 60  # Fotogramas por segundo
+BLANCO = (255, 255, 255)  # Color del fondo de la ventana (RGB)
