566865fd3b1041fe42a8e5952a2c49ef7b1646b6
paulaanb <paulanb2000@gmail.com>
2021-11-30 17:55:00 +0100

    Definimos las funciones mover, rebotar y reiniciar

--- paso5.py
+++ paso5.py
@@ -34,4 +34,22 @@ class PelotaPong:
         self.dir_x = random.choice([-5, 5])
         self.dir_y = random.choice([-5, 5])
         
-        
+    def mover(self):
+        self.x += self.dir_x
+        self.y += self.dir_y
+
+    def rebotar(self):
+        if self.x <= -self.ancho:
+            self.reiniciar()
+        if self.x >= VENTANA_HORI:
+            self.reiniciar()
+        if self.y <= 0:
+            self.dir_y = -self.dir_y
+        if self.y + self.alto >= VENTANA_VERT:
+            self.dir_y = -self.dir_y
+
+    def reiniciar(self):
+        self.x = VENTANA_HORI / 2 - self.ancho / 2
+        self.y = VENTANA_VERT / 2 - self.alto / 2
+        self.dir_x = -self.dir_x
+        self.dir_y = random.choice([-5, 5])
