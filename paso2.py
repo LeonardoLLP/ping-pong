58437755c6cde6621e6957b8ad354acbb8e3a5b1
paulaanb <paulanb2000@gmail.com>
2021-11-30 11:58:17 +0100

    Definimos y organizamos las constantes

---
+++ paso2.py
@@ -0,0 +1,16 @@
+# Empezamos a definir  la pelota
+# pong_1_2.py: Clase PelotaPong
+
+import random
+import pygame
+from pygame.locals import QUIT
+
+#Definimos las constantes para la inicialización de la superficie de dibujo
+# Ancho de la ventana
+VENTANA_HORI = 800
+# Alto de la ventana
+VENTANA_VERT = 600  
+# Fotogramas por segundo
+FPS = 60  
+# Color del fondo de la ventana (RGB)
+BLANCO = (255, 255, 255)  
