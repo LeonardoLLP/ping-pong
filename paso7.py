57ac3dc2102ff07f30010e69b9ec22e6ba041c50
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-11-30 12:09:48 +0100

    detectar el golpe de la pelota

--- paso7.py
+++ paso7.py
@@ -9,4 +9,5 @@ def golpear(self, pelota):
             pelota.dir_x = -pelota.dir_x
             pelota.x = self.x + self.ancho
 
-#
+# Detectar el golpe de la pelota
+        raqueta_1.golpear(pelota)
