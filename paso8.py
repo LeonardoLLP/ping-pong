f86736e268682c98be143586daf208ab762273bc
paulaanb <paulanb2000@gmail.com>
2021-11-30 18:36:45 +0100

    Estrategia para mover la pelota

---
+++ paso8.py
@@ -0,0 +1,13 @@
+#Al paso 7 debemos añadirle lo siguiente:
+#Estrategia para mover la raqueta
+def mover_ia(self, pelota):
+        if self.y > pelota.y:
+            self.dir_y = -3
+        elif self.y < pelota.y:
+            self.dir_y = 3
+        else:
+            self.dir_y = 0
+
+        self.y += self.dir_y
+
+#
