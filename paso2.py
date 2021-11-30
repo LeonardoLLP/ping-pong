bf94f4447837b32247881da01d9d9e58d4a1d59d
paulaanb <paulanb2000@gmail.com>
2021-11-30 18:16:15 +0100

    Definimos la Clase PelotaPong y sus atributos imagen, dimensión, posición y dirección de movimiento

--- paso2.py
+++ paso2.py
@@ -2,7 +2,24 @@
 #Importamos los módulos
 import random
 
+#Definimos la Clase PelotaPong y sus atributos
+class PelotaPong:
+    def __init__(self, fichero_imagen):
+        # --- Atributos de la Clase ---
 
+        # Imagen de la Pelota
+        self.imagen = pygame.image.load(fichero_imagen).convert_alpha()
+
+        # Dimensiones de la Pelota
+        self.ancho, self.alto = self.imagen.get_size()
+
+        # Posición de la Pelota
+        self.x = VENTANA_HORI / 2 - self.ancho / 2
+        self.y = VENTANA_VERT / 2 - self.alto / 2
+
+        # Dirección de movimiento de la Pelota
+        self.dir_x = random.choice([-5, 5])
+        self.dir_y = random.choice([-5, 5])
 
 
 
